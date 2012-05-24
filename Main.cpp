ea2e366b90372cc723acb68adbf776d8f19ca79b
Kim Oliver Rinnewitz <krinnewitz@uos.de>
2012-05-24 11:15:29 +0200

    Prepared code skeleton

--- Main.cpp
+++ Main.cpp
@@ -17,18 +17,35 @@
 
 using namespace std;
 
+/**
+ * \brief	Compares two images using SURF features
+ *
+ * \param	img1	The fist image
+ * \param	img2	The second image
+ *
+ * \return	A value between 0 and 1 indicating the similarity of
+ *		the input images. 0 means no similarity and 1 means
+ *		that both images are identical.
+ */
+double compareImages(cv::Mat &img1, cv::Mat &img2)
+{
+	return 0;
+}
 
 int main (int argc, char** argv)
 {
 
 	if (argc != 3)
 	{
-		cout<<"Usage: "<<argv[0]<<" <filename> <minimal pattern size>"<<endl;
+		cout<<"Usage: "<<argv[0]<<" <filename1> <filename2>"<<endl;
 		return EXIT_FAILURE;	
 	}
 	else
 	{
-		cv::Mat src = cv::imread(argv[1]);
+		cv::Mat img1 = cv::imread(argv[1]);
+		cv::Mat img2 = cv::imread(argv[2]);
+
+		cout<<compareImages(img1, img2)<<endl;
 
 		return EXIT_SUCCESS;
 	}
