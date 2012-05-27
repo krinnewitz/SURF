13d7f0348f7721be59dad78081b11d2deb652590
Kim Oliver Rinnewitz <krinnewitz@uos.de>
2012-05-27 15:07:50 +0200

    Added a heuristic method to improve the matching of descriptors

--- Main.cpp
+++ Main.cpp
@@ -17,7 +17,6 @@
 
 using namespace std;
 
-
 /**
  * \brief	Compares the two given sets of float vectors. It matches the
  *		first vector of the first set with the best fitting vector of
@@ -27,22 +26,21 @@ using namespace std;
  *		This method WON't find an optimal matching between the two
  *		sets of vectors. Better results may be achieved by heuristics.
  *
- * \param	descriptors1	The first set of vectors (="descriptors")
- * \param	descriptors2	The second set of vectors (="descriptors")
+ * \param	less		The smaller set of vectors (="descriptors")
+ * \param	more		The bigger set of vectors (="descriptors")
  * \param	descriptorSize	The length of every vector in the sets. 
+ * \param	matching	An array to hold the found matchings
  *
  * \return	The sum of all squared distances in the resulting matching.
  */
-double matchDescriptors(vector<float> descriptors1, vector<float> descriptors2, const int descriptorSize)
+double matchDescriptors(vector<float> less, vector<float> more, const int descriptorSize, int matching[])
 {
 	//This will hold the sum of all distances
 	double result = 0;
-	
-	// less points to the smaller vector
-	vector<float> &less = descriptors1.size() <  descriptors2.size() ? descriptors1 : descriptors2;
 
-	// more points to the bigger vector
-	vector<float> &more = descriptors1.size() >= descriptors2.size() ? descriptors1 : descriptors2;
+	//keeps track of the matched vectors to avoid collisions
+	bool* matched = new bool[more.size()];
+	for (int i = 0; i < more.size(); i++) matched[i] = false;
 
 	for (int l = 0; l < less.size(); l+= descriptorSize)
 	{
@@ -51,28 +49,92 @@ double matchDescriptors(vector<float> descriptors1, vector<float> descriptors2, 
 		//search best match
 		for (int m = 0; m < more.size(); m+= descriptorSize)
 		{
-			//calculate squared euclidean distance 
-			double distance = 0;
-			for (int v = 0; v < descriptorSize; v++)
+			if (! matched[m])
 			{
-				distance += (less[l+v] - more[m+v]) * (less[l+v] - more[m+v]);
-			}
+				//calculate squared euclidean distance 
+				double distance = 0;
+				for (int v = 0; v < descriptorSize; v++)
+				{
+					distance += (less[l+v] - more[m+v]) * (less[l+v] - more[m+v]);
+				}
 
-			if (distance < bestDistance)
-			{
-				bestDistance = distance;
-				bestMatchIndex = m;
+				if (distance < bestDistance)
+				{
+					bestDistance = distance;
+					bestMatchIndex = m;
+				}
 			}
 		}
-		
+		//save matching
+		matching[l] = bestMatchIndex;
+		//mark matched item as matched to avoid collisions 
+		matched[matching[l]] = true;
+		//increase the sum of squared distances
 		result += bestDistance;
-	
-		//erase match to avoid collisions 
-		more.erase(more.begin() + bestMatchIndex, more.begin() + bestMatchIndex + descriptorSize);	
 	}
 	return result;
 }
 
+/**
+ * \brief	Tries to improve the given matching by random swaps
+ *
+ * \param	less		The smaller set of vectors (="descriptors")
+ * \param	more		The bigger set of vectors (="descriptors")
+ * \param	descriptorSize	The length of every vector in the sets. 
+ * \param	matching	An array to hold the optimized matchings
+ *
+ * \return	The sum of all squared distances in the resulting matching.
+ */
+double improveMatching(vector<float> less, vector<float> more, const int descriptorSize, int matching[])
+{
+	//This will hold the sum of all distances
+	double result = 0;
+	//calculate current sum of squared distances
+	for (int l = 0; l < less.size(); l+= descriptorSize)
+	{
+		for (int v = 0; v < descriptorSize; v++)
+		{
+			result += (less[l+v] - more[matching[l]+v]) * (less[l+v] - more[matching[l]+v]);
+		}
+	}
+
+	//try to improve the matching
+	for (int i = 0; i < 10000; i++)
+	{
+		//choose two random vectors from the "less" set
+		int rnd1 = (rand() % (less.size() / descriptorSize)) * descriptorSize;
+		int rnd2 = (rand() % (less.size() / descriptorSize)) * descriptorSize;
+		
+		//swap them and check if this leads to a better distance
+		double newResult = result;	
+		for (int v = 0; v < descriptorSize; v++)
+		{
+			newResult -= (less[rnd1+v] - more[matching[rnd1]+v]) * (less[rnd1+v] - more[matching[rnd1]+v]);
+			newResult -= (less[rnd2+v] - more[matching[rnd2]+v]) * (less[rnd2+v] - more[matching[rnd2]+v]);
+		}
+		int tmp = matching[rnd1];
+		matching[rnd1] = matching[rnd2];
+		matching[rnd2] = tmp;
+		for (int v = 0; v < descriptorSize; v++)
+		{
+			newResult += (less[rnd1+v] - more[matching[rnd1]+v]) * (less[rnd1+v] - more[matching[rnd1]+v]);
+			newResult += (less[rnd2+v] - more[matching[rnd2]+v]) * (less[rnd2+v] - more[matching[rnd2]+v]);
+		}
+		if (newResult <= result)
+		{	
+			//found an improvement
+			result = newResult;
+		}
+		else
+		{
+			//undo the swap
+			matching[rnd2] = matching[rnd1];
+			matching[rnd1] = tmp;
+		}
+	}
+
+	return result;
+}
 /**
  * \brief	Compares two images using SURF features
  *
@@ -108,7 +170,15 @@ double compareImagesSURF(cv::Mat &image1, cv::Mat &image2, double ht)
 	surf(img2, cv::Mat(), keyPoints2, descriptors2);	
 
 	//compare features
-	return matchDescriptors(descriptors1, descriptors2, surf.descriptorSize());
+	// less points to the smaller vector
+	vector<float> &less = descriptors1.size() <  descriptors2.size() ? descriptors1 : descriptors2;
+	// more points to the bigger vector
+	vector<float> &more = descriptors1.size() >= descriptors2.size() ? descriptors1 : descriptors2;
+	double result; int* matching = new int[less.size()];
+	result = matchDescriptors(less, more, surf.descriptorSize(), matching);
+	result = improveMatching (less, more, surf.descriptorSize(), matching);
+
+	return result;
 }
 
 int main (int argc, char** argv)
