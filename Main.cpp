f889e545b44f80300eba81a8b1eb8005a73b5bf7
Kim Oliver Rinnewitz <krinnewitz@uos.de>
2012-06-09 16:57:08 +0200

    Feature descriptors are now matched by FLANN and results are more reliable

--- Main.cpp
+++ Main.cpp
@@ -18,123 +18,48 @@
 using namespace std;
 
 /**
- * \brief	Compares the two given sets of float vectors. It matches the
- *		first vector of the first set with the best fitting vector of
- *		the second set and then matches the second vector of the first
- *		set with the best fitting vector of the second set excluding
- *		the vector that has already been matched and so on.
- *		This method WON't find an optimal matching between the two
- *		sets of vectors. Better results may be achieved by heuristics.
+ * \brief	Matches the two given sets of feature descriptors and 
+ *		calculates a value representing the similarity of the 
+ *		associated images. This is done by considering only 
+ *		"good" matches i.e. matches with a small distance.
  *
- * \param	less		The smaller set of vectors (="descriptors")
- * \param	more		The bigger set of vectors (="descriptors")
- * \param	descriptorSize	The length of every vector in the sets. 
- * \param	matching	An array to hold the found matchings
+ * \param 	descriptors1	A matrix holding all descriptors of the
+ *				first image
+ * \param	descriptors2	A matrix holding all descriptors of the
+ *				second image
  *
  * \return	The sum of all squared distances in the resulting matching.
  */
-double matchDescriptors(vector<float> less, vector<float> more, const int descriptorSize, int matching[])
+float matchDescriptors(cv::Mat descriptors1, cv::Mat descriptors2)
 {
-	//This will hold the sum of all distances
-	double result = 0;
+	float result = 0;
+	
+	//calculate matching
+	cv::FlannBasedMatcher matcher;
+	vector< cv::DMatch > matches;
+	matcher.match( descriptors1, descriptors2, matches);
 
-	//keeps track of the matched vectors to avoid collisions
-	bool* matched = new bool[more.size()];
-	for (int i = 0; i < more.size(); i++) matched[i] = false;
-
-	for (int l = 0; l < less.size(); l+= descriptorSize)
-	{
-		int bestMatchIndex = 0;
-		double bestDistance = FLT_MAX;
-		//search best match
-		for (int m = 0; m < more.size(); m+= descriptorSize)
-		{
-			if (! matched[m])
-			{
-				//calculate squared euclidean distance 
-				double distance = 0;
-				for (int v = 0; v < descriptorSize; v++)
-				{
-					distance += (less[l+v] - more[m+v]) * (less[l+v] - more[m+v]);
-				}
-
-				if (distance < bestDistance)
-				{
-					bestDistance = distance;
-					bestMatchIndex = m;
-				}
-			}
-		}
-		//save matching
-		matching[l] = bestMatchIndex;
-		//mark matched item as matched to avoid collisions 
-		matched[matching[l]] = true;
-		//increase the sum of squared distances
-		result += bestDistance;
+	//search best match
+	double minDist = 100;
+	for (int i = 0; i < descriptors1.rows; i++)
+	{ 
+		if(matches[i].distance < minDist) minDist = matches[i].distance;
 	}
+
+	//Calculate result. Only good matches are considered.
+	int numGoodMatches = 0;
+	for( int i = 0; i < matches.size(); i++ )
+	{ 
+		if(matches[i].distance < 2 * minDist)
+		{
+			result += matches[i].distance * matches[i].distance;
+			numGoodMatches++;
+		}
+	}
+	result /= numGoodMatches;
 	return result;
 }
 
-/**
- * \brief	Tries to improve the given matching by random swaps
- *
- * \param	less		The smaller set of vectors (="descriptors")
- * \param	more		The bigger set of vectors (="descriptors")
- * \param	descriptorSize	The length of every vector in the sets. 
- * \param	matching	An array to hold the optimized matchings
- *
- * \return	The sum of all squared distances in the resulting matching.
- */
-double improveMatching(vector<float> less, vector<float> more, const int descriptorSize, int matching[])
-{
-	//This will hold the sum of all distances
-	double result = 0;
-	//calculate current sum of squared distances
-	for (int l = 0; l < less.size(); l+= descriptorSize)
-	{
-		for (int v = 0; v < descriptorSize; v++)
-		{
-			result += (less[l+v] - more[matching[l]+v]) * (less[l+v] - more[matching[l]+v]);
-		}
-	}
-
-	//try to improve the matching
-	for (int i = 0; i < 10000; i++)
-	{
-		//choose two random vectors from the "less" set
-		int rnd1 = (rand() % (less.size() / descriptorSize)) * descriptorSize;
-		int rnd2 = (rand() % (less.size() / descriptorSize)) * descriptorSize;
-		
-		//swap them and check if this leads to a better distance
-		double newResult = result;	
-		for (int v = 0; v < descriptorSize; v++)
-		{
-			newResult -= (less[rnd1+v] - more[matching[rnd1]+v]) * (less[rnd1+v] - more[matching[rnd1]+v]);
-			newResult -= (less[rnd2+v] - more[matching[rnd2]+v]) * (less[rnd2+v] - more[matching[rnd2]+v]);
-		}
-		int tmp = matching[rnd1];
-		matching[rnd1] = matching[rnd2];
-		matching[rnd2] = tmp;
-		for (int v = 0; v < descriptorSize; v++)
-		{
-			newResult += (less[rnd1+v] - more[matching[rnd1]+v]) * (less[rnd1+v] - more[matching[rnd1]+v]);
-			newResult += (less[rnd2+v] - more[matching[rnd2]+v]) * (less[rnd2+v] - more[matching[rnd2]+v]);
-		}
-		if (newResult <= result)
-		{	
-			//found an improvement
-			result = newResult;
-		}
-		else
-		{
-			//undo the swap
-			matching[rnd2] = matching[rnd1];
-			matching[rnd1] = tmp;
-		}
-	}
-
-	return result;
-}
 /**
  * \brief	Compares two images using SURF features
  *
@@ -142,9 +67,9 @@ double improveMatching(vector<float> less, vector<float> more, const int descrip
  * \param	img2	The second image. Must be a 3 channel RGB image.
  * \param	ht	The hessian Threshold used in the SURF algorithm
  *
- * \return	A value between 0 and 1 indicating the similarity of the
- * 		input images. 0 means no similarity and 1 means that both
- *		images are identical.
+ * \return	A value indicating the distance between the both images.
+ *		0 means that both images are identical. The higher the
+ *		distance, the lower the similarity of the images.
  */
 double compareImagesSURF(cv::Mat &image1, cv::Mat &image2, double ht)
 {
@@ -156,27 +81,27 @@ double compareImagesSURF(cv::Mat &image1, cv::Mat &image2, double ht)
 	cv::Mat img2;
 	cv::cvtColor(image2, img2, CV_RGB2GRAY);
 	
-	//initialize SURF object	
-	cv::SURF surf(ht);
-	
+	//initialize SURF objects
+	cv::SurfFeatureDetector detector(ht);
+	cv::SurfDescriptorExtractor extractor;
+
+	vector<cv::KeyPoint> keyPoints1, keyPoints2;
+	cv::Mat descriptors1, descriptors2;
+
 	//calculate SURF features for the first image
-	vector<cv::KeyPoint> keyPoints1;
-	vector<float> descriptors1;
-	surf(img1, cv::Mat(), keyPoints1, descriptors1);	
-	
+	detector.detect( img1, keyPoints1 );
+	extractor.compute( img1, keyPoints1, descriptors1 );
+
 	//calculate SURF features for the second image
-	vector<cv::KeyPoint> keyPoints2;
-	vector<float> descriptors2;
-	surf(img2, cv::Mat(), keyPoints2, descriptors2);	
+	detector.detect( img2, keyPoints2 );
+	extractor.compute( img2, keyPoints2, descriptors2 );
 
 	//compare features
-	// less points to the smaller vector
-	vector<float> &less = descriptors1.size() <  descriptors2.size() ? descriptors1 : descriptors2;
-	// more points to the bigger vector
-	vector<float> &more = descriptors1.size() >= descriptors2.size() ? descriptors1 : descriptors2;
-	double result; int* matching = new int[less.size()];
-	result = matchDescriptors(less, more, surf.descriptorSize(), matching);
-	result = improveMatching (less, more, surf.descriptorSize(), matching);
+	float result = FLT_MAX;
+	if (keyPoints1.size() > 0 && keyPoints2.size() > 0)
+	{
+		result = matchDescriptors(descriptors1, descriptors2);
+	}
 
 	return result;
 }
@@ -186,7 +111,7 @@ int main (int argc, char** argv)
 
 	if (argc != 4)
 	{
-		//try 500 or 1000 for hessian threshold
+		//try 100, 500 or 1000 for hessian threshold
 		cout<<"Usage: "<<argv[0]<<" <filename1> <filename2> <hessianThreshold>"<<endl;
 		return EXIT_FAILURE;	
 	}
